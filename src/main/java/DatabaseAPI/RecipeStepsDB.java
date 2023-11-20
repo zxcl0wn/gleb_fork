8243ce389d6368c9bb3942d7e9afa0e6ec40b513
Sergey <pristup2040@gmail.com>
2023-11-20 12:12:44 +1000

    add RecipeStepsDB

---
+++ src/main/java/DatabaseAPI/RecipeStepsDB.java
@@ -0,0 +1,84 @@
+package DatabaseAPI;
+
+import java.sql.*;
+
+
+abstract class RecipeStepsDBAbstract extends DB_BaseAbstract {
+    public RecipeStepsDBAbstract(String tableName) {
+        super(tableName);
+    }
+
+    public abstract void create(int recipe_id, String text, String img);
+
+    public abstract void update(int id, int recipe_id, String text, String img);
+}
+
+public class RecipeStepsDB extends RecipeStepsDBAbstract {
+    public RecipeStepsDB() {
+        super("Recipe");
+    }
+
+    public void createTableIfNotExists() {
+        try (Connection connection = this.getConnection()) {
+            String createTableQuery = String.format("CREATE TABLE IF NOT EXISTS %s (" +
+                    "id SERIAL PRIMARY KEY," +
+                    "recipe_id INTEGER REFERENCES Recipe (id) ON DELETE CASCADE," +
+                    "text TEXT," +
+                    "img TEXT" +
+                    ");", this.tableName);
+
+            Statement stmt = connection.createStatement();
+            stmt.executeUpdate(createTableQuery);
+
+        } catch (SQLException e) {
+            System.out.println("Error connecting to PostgreSQL database:");
+            e.printStackTrace();
+        }
+    }
+
+    public void initTable() {
+        RecipeDB recipe_db =  new RecipeDB();
+        recipe_db.initTable();
+
+        this.createTableIfNotExists();
+    }
+
+    public void create(int recipe_id, String text, String img) {
+        try (Connection connection = this.getConnection()) {
+            String insertQuery = String.format(
+                    "INSERT INTO %s (recipe_id, text, img) VALUES (?, ?, ?);",
+                    this.tableName
+            );
+
+            PreparedStatement pstmt = connection.prepareStatement(insertQuery);
+            pstmt.setInt(1, recipe_id);
+            pstmt.setString(2, text);
+            pstmt.setString(3, img);
+            pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            System.out.println("Error connecting to PostgreSQL database:");
+            e.printStackTrace();
+        }
+
+    }
+
+    public void update(int id, int recipe_id, String text, String img) {
+        try (Connection connection = this.getConnection()) {
+            String updateQuery = String.format(
+                    "UPDATE %s SET recipe_id = ?, text = ?, img = ? WHERE id = ?;",
+                    this.tableName
+            );
+            PreparedStatement pstmt = connection.prepareStatement(updateQuery);
+            pstmt.setInt(1, recipe_id);
+            pstmt.setString(2, text);
+            pstmt.setString(3, img);
+            pstmt.setInt(4, id);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Error connecting to PostgreSQL database:");
+            e.printStackTrace();
+        }
+    }
+
+}
