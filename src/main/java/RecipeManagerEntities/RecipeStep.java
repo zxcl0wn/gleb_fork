eea4ebf56dd4bfd5bc7df215dd3d6596bfccc5f6
Sergey <pristup2040@gmail.com>
2023-11-24 11:28:53 +1000

    start RecipeStep

--- src/main/java/RecipeManagerEntities/RecipeStep.java
+++ src/main/java/RecipeManagerEntities/RecipeStep.java
@@ -1,10 +1,35 @@
 package RecipeManagerEntities;
 
 public class RecipeStep {
-    private int id;
-    private Recipe recipe;
-    private String text;
-    private String img;
+    public int getId() {
+        return id;
+    }
 
-    // getters and setters
+    public String getRecipeId() {
+        return recipe_id;
+    }
+
+    private int id;
+    private String recipe_id;
+    public String text;
+    public String img;
+
+    public RecipeStep(int id, String recipe_id, String text, String img) {
+        this.id = id;
+        this.recipe_id = recipe_id;
+        this.text = text;
+        this.img = img;
+    }
+
+    @Override
+    public String toString() {
+        return "RecipeStep{" +
+                "id=" + id +
+                ", recipe=" + recipe_id +
+                ", text='" + text + '\'' +
+                ", img='" + img + '\'' +
+                '}';
+    }
+
+
 }
