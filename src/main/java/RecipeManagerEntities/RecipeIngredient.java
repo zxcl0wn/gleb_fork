4e9d67c8651e169d9b48bdd6e5fa90ad41194c2d
Sergey <pristup2040@gmail.com>
2023-11-27 13:07:27 +1000

    start RecipeIngredient

--- src/main/java/RecipeManagerEntities/RecipeIngredient.java
+++ src/main/java/RecipeManagerEntities/RecipeIngredient.java
@@ -2,9 +2,50 @@ package RecipeManagerEntities;
 
 public class RecipeIngredient {
     private int id;
-    private Recipe recipe;
-    private Ingredient ingredient;
+    private int recipe_id;
+    private int ingredient_id;
     private double quantityOfIngredient;
+
+    private RecipeIngredient(int id, int recipe_id, int ingredient_id, double quantityOfIngredient) {
+        this.id = id;
+        this.recipe_id = recipe_id;
+        this.ingredient_id = ingredient_id;
+        this.quantityOfIngredient = quantityOfIngredient;
+    }
+
+
+
+
+
+    @Override
+    public String toString() {
+        return "RecipeIngredient{" +
+                "id=" + id +
+                ", recipe_id=" + recipe_id +
+                ", ingredient_id=" + ingredient_id +
+                ", quantityOfIngredient=" + quantityOfIngredient +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getRecipeId() {
+        return recipe_id;
+    }
+
+    public int getIngredientId() {
+        return ingredient_id;
+    }
+
+    public double getQuantityOfIngredient() {
+        return quantityOfIngredient;
+    }
+
+    public void setQuantityOfIngredient(double quantityOfIngredient) {
+        this.quantityOfIngredient = quantityOfIngredient;
+    }
 }
 
 
