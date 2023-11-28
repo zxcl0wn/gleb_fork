e3ac1eb7d2d81f9835fa166b37e0029f5c55da79
Sergey <pristup2040@gmail.com>
2023-11-28 16:41:04 +1000

    remove throws SQLException in ShoppingCart.java

--- src/main/java/RecipeManagerEntities/ShoppingCart.java
+++ src/main/java/RecipeManagerEntities/ShoppingCart.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.*;
 
+
 public class ShoppingCart {
     private static ResultSet getShoppingCartIdsResultSet(){
         ShoppingCartDB shopping_cart_db = new ShoppingCartDB();
@@ -17,32 +18,36 @@ public class ShoppingCart {
         return shopping_cart_db.read(id);
     }
 
-    private static List<Integer> getShoppingCartIdsByResultSet(ResultSet rs) throws SQLException {
+    private static List<Integer> getShoppingCartIdsByResultSet(ResultSet rs) {
         List<Integer> list = new LinkedList<>();
-        while (rs.next()) {
-            int id = rs.getInt("id");
-            int recipe_ingredient_id = rs.getInt("recipe_ingredient_id");
-            list.add(recipe_ingredient_id);
+        try {
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                int recipe_ingredient_id = rs.getInt("recipe_ingredient_id");
+                list.add(recipe_ingredient_id);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
         return list;
     }
 
-    private static List<Integer> getShoppingCartIds() throws SQLException {
+    private static List<Integer> getShoppingCartIds() {
         return getShoppingCartIdsByResultSet(getShoppingCartIdsResultSet());
     }
 
-    private static List<Integer> getShoppingCartItemById(int id) throws SQLException {
+    private static List<Integer> getShoppingCartItemById(int id) {
         // по первичному ключу в бд ShoppingCart
         return getShoppingCartIdsByResultSet(getShoppingCartItemByIdResultSet(id));
     }
 
-    public static boolean checkIsInDB(int recipe_ingredient_id) throws SQLException {
+    public static boolean checkIsInDB(int recipe_ingredient_id) {
         // по клонке recipe_ingredient_id в бд ShoppingCart
         List<Integer> shopping_cart_items = getShoppingCartIds();
         return shopping_cart_items.contains(recipe_ingredient_id);
     }
 
-    public static void addIngredientsToShoppingCartByRecipe(Recipe recipe) throws SQLException {
+    public static void addIngredientsToShoppingCartByRecipe(Recipe recipe) {
         List<IngredientWithQuantity> ingredients_with_quantity = recipe.getIngredientsWithQuantity();
         ShoppingCartDB shopping_cart_db = new ShoppingCartDB();
 
@@ -52,14 +57,12 @@ public class ShoppingCart {
                     ingredient_with_quantity.getIngredient().getId()
             );
             if (Objects.isNull(recipe_ingredient)) continue;
-//            System.out.println("-");
-//            System.out.println(recipe_ingredient);
-//            System.out.println(!checkIsInDB(recipe_ingredient.getId()));
+
             if (!checkIsInDB(recipe_ingredient.getId())) shopping_cart_db.create(recipe_ingredient.getId());
         }
     }
 
-    public static Map<String, Double> getIngredientsMap () throws SQLException {
+    public static Map<String, Double> getIngredientsMap () {
         Map<String, Double> map = new HashMap<>();
         List<Integer> ingredients = getShoppingCartIds();
 
