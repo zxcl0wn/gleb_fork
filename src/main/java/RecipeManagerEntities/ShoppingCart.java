b0bc8ca27b6e56cd59d8074459fbd1b9d3a0a7e1
Sergey <pristup2040@gmail.com>
2023-11-28 15:06:04 +1000

    do ShoppingCart

--- src/main/java/RecipeManagerEntities/ShoppingCart.java
+++ src/main/java/RecipeManagerEntities/ShoppingCart.java
@@ -1,27 +1,31 @@
 package RecipeManagerEntities;
 
+import DatabaseAPI.ShoppingCartDB;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+import java.util.HashSet;
+
 public class ShoppingCart {
-    private final int id;
-    private final int recipe_ingredient_id;
-
-    private ShoppingCart(int id, int recipe_ingredient_id) {
-        this.id = id;
-        this.recipe_ingredient_id = recipe_ingredient_id;
+    private static ResultSet getShoppingCartResultSet(){
+        ShoppingCartDB shopping_cart_db = new ShoppingCartDB();
+        return shopping_cart_db.readAll();
     }
 
-    @Override
-    public String toString() {
-        return "ShoppingCart{" +
-                "id=" + id +
-                ", recipe_ingredient_id=" + recipe_ingredient_id +
-                '}';
+    private static List<Integer> getShoppingCartRecipeIngredientIdByResultSet(ResultSet rs) throws SQLException {
+        List<Integer> list = new LinkedList<>();
+        while (rs.next()) {
+            int id = rs.getInt("id");
+            int recipe_ingredient_id = rs.getInt("recipe_ingredient_id");
+            list.add(recipe_ingredient_id);
+        }
+        return list;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public int getRecipeIngredientId() {
-        return recipe_ingredient_id;
+    public boolean addIngredientsToShoppingCartByRecipe(Recipe recipe) {
+        return true;
     }
 }
